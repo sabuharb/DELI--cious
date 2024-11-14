64fcc46c99708e09880eaf5f4e302725f044e136
sabuharb <abuharb.sandy@gmail.com>
2024-11-14 00:09:19 -0600

    added code to Chips Class

--- DELI--cious/src/main/java/com/pluralsight/Chips.java
+++ DELI--cious/src/main/java/com/pluralsight/Chips.java
@@ -1,6 +1,23 @@
 package com.pluralsight;
 
+import java.lang.reflect.Type;
+
 public class Chips {
     // Propiedades de los chips
-    private 
+    private String type;
+    private double price = 1.5;
+
+    //constructor
+
+    public Chips(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Type of chips: " + Type);
+    }
 }
