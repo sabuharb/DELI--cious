d789b5c090690198ddfc9ff95426fed0cc825ca8
sabuharb <abuharb.sandy@gmail.com>
2024-11-14 01:53:55 -0600

    Deleted what I had and started it over, added code to sandwich and created class for drink and working on code now

--- DELI--cious/src/main/java/com/pluralsight/Drink.java
+++ DELI--cious/src/main/java/com/pluralsight/Drink.java
@@ -1,33 +1,23 @@
 package com.pluralsight;
 
 public class Drink {
-    // Propiedades de la bebidas
     private String size;
-    private double price;
 
-    // Constructor
     public Drink(String size) {
         this.size = size;
-        this.price = determinePrice();
-    }
-
-    // Metodo para determinar el precio segun el tamanio de la bebida
-    private double determinePrice() {
-        switch (size) {
-            case "Small": return 2.0;
-            case "Medium": return 2.5;
-            case "Large": return 3.0;
-            default: return 0;
-        }
     }
 
     public double getPrice() {
-        return price;
+        switch (size) {
+            case "Small": return 2.00;
+            case "Medium": return 2.50;
+            case "Large": return 3.00;
+            default: return 0;
+        }
     }
 
     public void displayDetails() {
         System.out.println("Drink size: " + size);
     }
 }
-
 
--- DELI--cious/src/main/java/com/pluralsight/Sandwich.java
+++ DELI--cious/src/main/java/com/pluralsight/Sandwich.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Sandwich {
+    private String size;
+    private String bread;
+    private ArrayList<String> toppings;
+    private boolean toasted;
+
+    // Constructor que comienza las propiedades del sandwich
+    public Sandwich(String size, String bread, ArrayList<String> toppings, boolean toasted) {
+        this.size = size;
+        this.bread = bread;
+        this.toppings = toppings;
+        this.toasted = toasted;
+    }
+
+    // Dertermina el precio del muy sandwich basado en el tamanio
+    public double getPrice() {
+        double price = 0
+        switch (size) {
+            case "4\"": return 5.50;
+            case "8\"": return 7.00;
+            case "12\"": return 8.50;
+        }
+        return price;
+    }
+
+    public void displayDetails(){
+        System.out.println(size + " sandwich on " + bread + " with toppings: " toppings);
+    }
 }
